8916bcc35da6c4fc1e3a5f6f1576e85ec6bede21
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-10-26 06:27:50 +0100

    add scroll funtionality

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const navlinks = document.querySelector('.slide-menu-container');
 const closeBtn = document.querySelector('.close-btn');
 const menuBar = document.querySelector('.mobile_menu');
 
+// menu slide interaction
+
 menuBar.addEventListener('click', () => {
   navlinks.classList.toggle('show-menu');
 });
@@ -9,6 +11,25 @@ menuBar.addEventListener('click', () => {
 closeBtn.addEventListener('click', (e) => {
   if (navlinks.classList.contains('show-menu')) {
     navlinks.classList.remove('show-menu');
-    console.log(closeBtn);
   }
+});
+
+// scroll interactions
+const scrollLinks = document.querySelectorAll('.scroll-links');
+
+scrollLinks.forEach((scroll) => {
+  scroll.addEventListener('click', (a) => {
+    const id = a.currentTarget.getAttribute('href');
+    const element = document.getElementById(id);
+    const position = element.offsetTop;
+
+    if (id) {
+      navlinks.classList.remove('show-menu');
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
 });
