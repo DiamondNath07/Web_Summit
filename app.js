b26575bf866ae4303dbdf63ebc850566aeedfc21
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-10-27 05:27:05 +0100

    add unique images

--- app.js
+++ app.js
@@ -40,18 +40,18 @@ const speaker = document.querySelector('.speakers-container');
 
 const eventSpeakers = [
   {
-    img: './image1/pic1.jpg',
-    fullName: 'Diamond Nathaniel',
-    position: 'Software engineer || Frontend developer || Accountant',
+    img: './image1/nich.jpg',
+    fullName: 'Diamond Nicholas',
+    position: 'Full Stack Engineer || Cloud Engineer || Mathematician ',
     contributions:
-      'He will be talking about the simplicity yet power weapon of tech and how it impacts us.',
+      'He will be talking about the  next big thing about the tech industry as well as the future of tech',
   },
   {
-    img: './images2/speaker_03.png',
-    fullName: 'SohYeoung Noh',
-    position: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: './image1/pic1.jpg',
+    fullName: 'Diamond Nathaniel',
+    position: 'Software engineer || Frontend dev || Accountant || Youth Influencer',
     contributions:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis a ratione laborum?',
+      'He will be talking about the simplicity but powerful weapon of tech and how it impacts us today.',
   },
 ];
 
@@ -66,7 +66,7 @@ eventSpeakers.forEach(({ img, fullName, position, contributions }) => {
   <img src="${img}" alt="" class="speaker-pic" />
           <div class="speaker-desc">
             <h6 class="name">${fullName}</h6>
-            <p class="position">
+            <p class="positionA">
               ${position}
             </p>
             <p class="contributions">
@@ -85,32 +85,32 @@ const speakerB = document.querySelector('.speakers-container2');
 
 const eventSpeakers2 = [
   {
-    img: './images2/speaker_01.png',
-    fullName: 'Diamond Nathaniel',
-    position: 'Software engineer || Frontend developer || Accountant',
+    img: './image1/evans.jpg',
+    fullName: 'Ubah Evans',
+    position: 'Economist ',
     contributions:
-      'He will be talking about the simplicity yet power weapon of tech and how it impacts us.',
+      'He will be addressing how we can effectively manage the downsides tech bring with respect to company downsizing and computers replacing us',
   },
   {
-    img: './images2/speaker_02.png',
-    fullName: 'SohYeoung Noh',
-    position: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: './image1/inno.jpg',
+    fullName: 'Diamond Innocent',
+    position: 'Data Scientist || Physicist ',
     contributions:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis a ratione laborum?',
+      'He will be sharing exclusive thoughts on the importance of effective data storage for all tech advancement both now and future',
   },
   {
-    img: './images2/speaker_06.png',
-    fullName: 'SohYeoung Noh',
-    position: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: './image1/blackstan.jpg',
+    fullName: 'Sunday Stanley',
+    position: 'Q.A Engineer || Frontend developer',
     contributions:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis a ratione laborum?',
+      'He will share his thoughts on the subject of quality assurance and how viable it is to texh productions both future wise and even now',
   },
   {
-    img: './images2/speaker_05.png',
-    fullName: 'SohYeoung Noh',
-    position: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: './image1/john.jpg',
+    fullName: 'Omachi John',
+    position: 'Frontend dev || Digital Markerter',
     contributions:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis a ratione laborum?',
+      'He will be saring the recent techs and frameworks that will match up the next phase the tech world is looming into',
   },
 ];
 
@@ -152,13 +152,13 @@ const showMore = `
 
 const dropDown = document.querySelector('.drop-down');
 dropDown.addEventListener('click', () => {
-  specialSpeakers.classList.toggle('show')
+  specialSpeakers.classList.toggle('show');
   console.log(specialSpeakers);
-  
+
   if (specialSpeakers.classList.contains('show')) {
-    dropDown.innerHTML = showMore;
-  } else {
     dropDown.innerHTML = showLess;
+  } else {
+    dropDown.innerHTML = showMore;
   }
   homeFooter.forEach((footer) => {
     footer.classList.toggle('open');
